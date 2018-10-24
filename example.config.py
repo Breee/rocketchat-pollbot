f97c8bf7f3a4e5ee7787442c19453cab60bc4566
Trigrab <trigrab@gmail.com>
2018-10-24 16:02:22 +0200

    change: default mensa url

--- example.config.py
+++ example.config.py
@@ -1,6 +1,6 @@
 BOTNAME = "testman"
 PASSWORD = "strong_pw"
 SERVER = "https://test.testserver.com"
-#MENSA_CACHE_URL = 'https://info.l-b-f.de/cache/mensa' #only for mensabot needed to fetch food.
+#MENSA_CACHE_URL = 'https://infomonitor.somwhere.com/json/mensa' #only for mensabot needed to fetch food.
 #MENSA_NAMES = ['Flugplatz', 'Rempartstrasse', 'Institusviertel']
 #DEFAULT_MENSA = 'Flugplatz'
